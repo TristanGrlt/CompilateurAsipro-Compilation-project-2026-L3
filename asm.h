1f8a388c1c5c8df8fe5bd60e604677b42190b60d
TristanGrlt <tristan.groult@univ-rouen.fr>
2026-02-10 14:22:53 +0100

    Refactor assembly macros for improved readability and consistency

--- asm.h
+++ asm.h
@@ -3,14 +3,27 @@
 #define cx "cx"
 #define dx "dx"
 
-#define pop(r) {printf("\tpop "r"\n");}
-#define push(r) {printf("\tpush "r"\n");}
+#define _(msg)                                                                 \
+  { printf(";;;;;;;;;; " msg " ;;;;;;;;;;\n"); }
 
-#define add(r1, r2) {printf("\tadd "r1","r2"\n");}
-#define sub(r1, r2) {printf("\tsub "r1","r2"\n");}
-#define mul(r1, r2) {printf("\tmul "r1","r2"\n");}
-#define div(r1, r2) {printf("\tdiv "r1","r2"\n");}
+#define pop(r)                                                                 \
+  { printf("\tpop " r "\n"); }
+#define push(r)                                                                \
+  { printf("\tpush " r "\n"); }
 
-#define const(r, v) {printf("\tconst "r","v"\n");}
+#define add(r1, r2)                                                            \
+  { printf("\tadd " r1 "," r2 "\n"); }
+#define sub(r1, r2)                                                            \
+  { printf("\tsub " r1 "," r2 "\n"); }
+#define mul(r1, r2)                                                            \
+  { printf("\tmul " r1 "," r2 "\n"); }
+#define div(r1, r2)                                                            \
+  { printf("\tdiv " r1 "," r2 "\n"); }
 
-#define jmpe(r) {printf("\tjmpe "r"\n");}
+#define const(r, v)                                                            \
+  { printf("\tconst " r "," v "\n"); }
+
+#define jmpe(r)                                                                \
+  { printf("\tjmpe " r "\n"); }
+
+#define toto
